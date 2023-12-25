8d620f58dbb6ed59ecf9827808c0f9fb108857ca
Dasha <girl.is.anime20@gmail.com>
2023-12-25 20:40:17 +0300

    feat: add remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let prev = null;
+  let heigh = l;
+
+  while (l) {
+    if (l.value === k) {
+      if (prev === null) {
+        heigh = l = l.next
+        continue
+      } else {
+        prev.next = l.next
+        l = l.next
+        continue
+      }
+    }
+    prev = l;
+    l = l.next;
+  }
+  return heigh;
 }
 
 module.exports = {
